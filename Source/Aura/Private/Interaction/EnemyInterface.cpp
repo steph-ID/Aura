d24bcd0da18ea955b0cbeefb89d2408378bad17f
Stephanie Bledsoe <106919710+steph-ID@users.noreply.github.com>
2023-06-25 02:41:33 -0400

    Enemy highlight interface & logic

---
+++ Source/Aura/Private/Interaction/EnemyInterface.cpp
@@ -0,0 +1,13 @@
+// Copyright AlicePlaysTerribly
+
+
+#include "Interaction/EnemyInterface.h"
+
+// Add default functionality here for any IEnemyInterface functions that are not pure virtual.
+void IEnemyInterface::HighlightActor()
+{
+}
+
+void IEnemyInterface::UnHighlightActor()
+{
+}
